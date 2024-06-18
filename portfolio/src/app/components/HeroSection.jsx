007fa2bc4c461abce4df341769fac069da3ec540
Shane Sharareh <shane.s.sharareh@gmail.com>
2024-06-18 10:52:33 -0700

    fix path error

---
+++ portfolio/src/app/components/HeroSection.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section>
+        <h1 className="text-white mb-4 text-6xl font-extrabold">Hello, I'm Shane</h1>
+        <p className="text-[#ADB7BE]">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, magni nulla. Necessitatibus culpa sapiente est amet provident assumenda. Magnam, fuga. In dicta omnis error minima aliquam quasi similique tempore enim
+        </p>
+    </section>
+  )
+}
+
+export default HeroSection
